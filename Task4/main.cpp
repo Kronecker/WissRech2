20903701e420a8371a45b5e9f2858e9e0395437a
Kronecker <benjamin.grabiger@uni-jena.de>
2018-06-08 11:16:00 +0200

    Starting Cuda Version

--- Task4/main.cpp
+++ Task4/main.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <math.h>
 #include <fstream>
+#include <cuda.h>
 
 //#define flouble float
 #define flouble double
 #define MAXITERATIONS 2000
 
 void aufg13a();
+flouble* initMatrixRightHandSide(int n, flouble h  );
+flouble* jacobiIter(int n, flouble *f, flouble valBoundary, int* numberOfIterations, flouble h);
+
 void aufg13b();
+flouble* jacobiIterCuda_CPU(int n, flouble *f, flouble valBoundary, int* numberOfIterations, flouble h);
+
 void aufg13c();
+
 void aufg13d();
 
+
+// Utility
 flouble* initMatrixKonstant(int m,int n, flouble value  ) ;
 void displayMyMatrix(flouble* matrix, int m,int n);
-flouble* initMatrixRightHandSide(int m,int n, flouble h  );
-flouble* jacobiIterOfBlockMatrixFourDiags(int n, flouble *f, flouble valBoundary, int* numberOfIterations, flouble h);
 void saveMyMatrix(flouble* matrix, int m,int n, flouble h);
 
 
@@ -30,14 +37,14 @@ void aufg13a() {
     flouble h = 1./(n-1);
 
     flouble boundaryValue=0;
-    flouble *fun=initMatrixRightHandSide(n,n,h);
+    flouble *fun;
     flouble *result;
 
     int doneIterations=0;
 
 
-
-    result=jacobiIterOfBlockMatrixFourDiags(n, fun, boundaryValue, &doneIterations,h);
+    fun=initMatrixRightHandSide(n,h);
+    result=jacobiIter(n, fun, boundaryValue, &doneIterations,h);
 
     std::cout<<fun[1]<<std::endl;
 
@@ -49,16 +56,16 @@ void aufg13a() {
     delete(result);
 }
 
-flouble* initMatrixRightHandSide(int m,int n, flouble h  ) {
-    flouble*matrix=new flouble[n*m];
+flouble* initMatrixRightHandSide(int n, flouble h  ) {
+    flouble*matrix=new flouble[n*n];
     flouble x;
     flouble y;
-    for (int i=0;i<m;i++) {
+    for (int i=0;i<n;i++) {
 
         for (int j=0;j<n;j++) {
             x=h*i;
             y=h*j;
-            matrix[i*m+j]=x*(1-x)+y*(1-y);
+            matrix[i*n+j]=x*(1-x)+y*(1-y);
    //         printf("<%f %f> %f\n",x,y,matrix[i*m+j]);
         }
     }
@@ -66,7 +73,7 @@ flouble* initMatrixRightHandSide(int m,int n, flouble h  ) {
 }
 
 // zu Aufgabe 13a
-flouble* jacobiIterOfBlockMatrixFourDiags(int n, flouble *f, flouble valBoundary, int* numberOfIterations, flouble h) {
+flouble* jacobiIter(int n, flouble *f, flouble valBoundary, int* numberOfIterations, flouble h) {
 
     flouble* actualIteration=new flouble[n*n]();
     flouble* lastIterSol=new flouble[n*n]();
@@ -138,6 +145,109 @@ flouble* jacobiIterOfBlockMatrixFourDiags(int n, flouble *f, flouble valBoundary
     delete(lastIterSol);
     return actualIteration;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+flouble* jacobiIterCuda_CPU(int n, flouble *f, flouble valBoundary, int* numberOfIterations, flouble h) {
+
+    flouble* actualIteration=new flouble[n*n]();
+
+
+    flouble* cuda_actualIteration;
+    cudaMalloc(&cuda_actualIteration,sizeof(flouble)*n*n);;
+    flouble* cuda_lastIterSol=new flouble[n*n]();
+    cudaMalloc(&cuda_lastIterSol,sizeof(flouble)*n*n);;
+    flouble *cuda_f;
+
+
+
+    flouble tol=0.0001;
+    int iteration=0;
+    flouble resi=tol+1;
+    int step=100;
+
+    flouble hsquare=h*h;
+    flouble valSubDiag=-1/hsquare;
+    flouble valMainDiag=4/hsquare;
+
+
+
+/*    // boundary values init (outer)
+    for(int i=0;i<n;i++) {
+        actualIteration[i]=valBoundary;
+        lastIterSol[i]=valBoundary;
+        actualIteration[n*(n-1)+i]=valBoundary;
+        lastIterSol[n*(n-1)+i]=valBoundary;
+    }
+    for(int k=1;k<n-1;k++) { // iterate through blocks
+        actualIteration[k*n]=valBoundary;
+        lastIterSol[k*n]=valBoundary;
+        actualIteration[(k+1)*n-1]=valBoundary;
+        lastIterSol[(k+1)*n-1]=valBoundary;
+    }
+
+
+    int nm1=n-1;
+    int index;
+    while(iteration<MAXITERATIONS&&resi>tol) {
+        // consecutive blocks
+
+
+
+
+        if (!(iteration % step)) {
+            resi=0;
+            for(int i=0;i<n*n;i++) {
+                resi+=fabs(actualIteration[i]- lastIterSol[i]);
+            }
+            //   std::cout << iteration <<": "<< resi<< std::endl;
+        }
+
+
+        temp=lastIterSol;
+        lastIterSol=actualIteration;
+        actualIteration=temp;
+        iteration++;
+
+
+    }
+    std::cout << "Calculation finished after "<<iteration<<" Iterations.(%"<<step<<")"<<std::endl;
+    *numberOfIterations=iteration;
+
+    delete(lastIterSol);
+    return actualIteration;*/
+}
+
+__global__ void cuda_jacoboIteration (flouble* actualIteration,flouble*  lastIterSol,int n,flouble valSubDiag,flouble  valMainDiag, flouble* f ) {
+/*    int index;  //index=k*n+i;
+    int tid=threadIdx.x;
+    int bid=blockIdx.x;
+    int bdim=blockDim.x;
+
+    for(int k=1;k<nm1;k++) { // iterate through blocks
+
+        for(int i=1;i<nm1;i++) {  // iterate in block
+            actualIteration[index]=1/valMainDiag*(f[index]-valSubDiag*lastIterSol[index-n]-valSubDiag*lastIterSol[index-1]-valSubDiag*lastIterSol[index+1]-valSubDiag*lastIterSol[index+n]);
+        }
+
+    }
+*/
+}
+
+
+
 
 
 void aufg13b() {
