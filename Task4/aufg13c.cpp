2994a6af5d6131e3efc355f1394cc6e0251d801d
Kronecker <benjamin.grabiger@uni-jena.de>
2018-06-10 16:00:29 +0200

    Aufg13c

--- Task4/aufg13c.cpp
+++ Task4/aufg13c.cpp
@@ -30,10 +30,10 @@ void aufg13c() {
 
     initMatrixRightHandSideCuda_1Core_CUDA<<<1,n>>>(h,cuda_fun,n);
 
-    result=jacobiIterCuda_CPU(n, cuda_fun, boundaryValue, &doneIterations,h);
+   // result=jacobiIterCuda_CPU(n, cuda_fun, boundaryValue, &doneIterations,h);
     cudaThreadExit();
 
-    saveMyMatrix(result, n,n,h,2);
+  //  saveMyMatrix(result, n,n,h,2);
 
 }
 
@@ -56,8 +56,8 @@ flouble* jacobiIterCuda_1Core_CPU(int n, flouble *cudaF, flouble valBoundary, in
     flouble tol=0.0001;
     int iteration=0;
     flouble resi=tol+1;
-    flouble *resiCuda;
-    cudaMalloc(&resiCuda,sizeof(flouble));
+    flouble *intas;
+    cudaMalloc(&itas,sizeof(int));
     int step=100;  // 2 Iterations
     int maxDoubleIter=MAXITERATIONS/2;
 
@@ -65,24 +65,11 @@ flouble* jacobiIterCuda_1Core_CPU(int n, flouble *cudaF, flouble valBoundary, in
     flouble valSubDiag=-1/hsquare;
     flouble valMainDiag=4/hsquare;
 
-
+    initSolutionVectors_1Core_CUDA<<<1,n>>>(cuda_actualIteration,cuda_lastIterSol,n,valSubDiag,valMainDiag,cudaF)
     while(iteration<maxDoubleIter) {
         // consecutive blocks
 
-        jacoboIteration_CUDA <<<n,n>>>(cuda_actualIteration,cuda_lastIterSol,n,valSubDiag,valMainDiag,cudaF);
-        jacoboIteration_CUDA <<<n,n>>>(cuda_lastIterSol,cuda_actualIteration,n,valSubDiag,valMainDiag,cudaF);
-        iteration++;
-        if(iteration%step==0) {
-            calculateResidual_CUDA <<<n,n>>>(cuda_actualIteration, cuda_lastIterSol, resiCuda);
-            cudaMemcpy(&resi,resiCuda,sizeof(flouble),cudaMemcpyDeviceToHost);
 
-            cout<<iteration*2<<": "<<resi<<endl;
-            if(resi<tol) {
-                break;
-            }
-            resi=0;  // Reset resiCuda.....is there any better way?
-            cudaMemcpy(resiCuda,&resi,sizeof(flouble),cudaMemcpyHostToDevice);
-        }
     }
     std::cout << "Calculation finished after "<<2*iteration<<" Iterations.(%"<<step<<")"<<std::endl;
     *numberOfIterations=iteration*2;
@@ -103,99 +90,153 @@ __global__ void initMatrixRightHandSideCuda_1Core_CUDA(flouble h, flouble* matri
         matrix[bid * blockDim.x + tid] = x * (1 - x) + y * (1 - y);
 
     }
-
+    calculateResidual_1Core_CUDA(nullptr,nullptr,nullptr,1);
 }
 
 __global__ void initSolutionVectors_1Core_CUDA(flouble *actualIteration, flouble valBoundary, int n) {
     int tid = threadIdx.x;
-    int bid = blockIdx.x;
 
+    actualIteration[tid] = valBoundary;
+    actualIteration[n * (n-1) + tid] = valBoundary;
 
-    if ((bid == 0)||(bid == n-1)) {  // boundary values init (outer)
-        actualIteration[n * bid + tid] = valBoundary;
-    } else {
-        if((tid==0)||tid==n-1) {
-            actualIteration[n * bid + tid] = valBoundary;
-        }else {
-            actualIteration[bid*n+tid] = 0;
-        }
+    for(int bid=1;bid<n-1;bid++) {
+            if ((tid == 0) || tid == n - 1) {
+                actualIteration[n * bid + tid] = valBoundary;
+            } else {
+                actualIteration[bid * n + tid] = 0;
+            }
+
     }
 }
 
 
 
 __global__ void jacoboIteration_1Core_CUDA(flouble *actualIteration, flouble *lastIterSol, int n, flouble valSubDiag,
-                                     flouble valMainDiag, flouble *f) {
+                                     flouble valMainDiag, flouble *f, int *iteration) {
     int index;  //index=k*n+i;
-    int tid=threadIdx.x;
-    int bid=blockIdx.x;
-    int bdim=blockDim.x;
+    int tid = threadIdx.x;
+    int bid = blockIdx.x;
+    int step=100;
 
-    if(bid==0||bid==gridDim.x-1) {  // Boundaries, nothing to do here
-        return;
-    }
-    if(tid==0||tid==gridDim.x-1) {  // Boundaries, nothing to do here
-        return;
-    }
 
-    index=bid*bdim+tid;
-    actualIteration[index]=1/valMainDiag*(f[index]-valSubDiag*lastIterSol[index-bdim]-valSubDiag*lastIterSol[index-1]-valSubDiag*lastIterSol[index+1]-valSubDiag*lastIterSol[index+bdim]);
+    for (bid = 1; bid < n-1; bid++) {
+
+        if (tid == 0 || tid == n - 1) {  // Boundaries, nothing to do here
+            continue;
+        }
+
+        index = bid * n + tid;
+        actualIteration[index] = 1 / valMainDiag *
+                                 (f[index] - valSubDiag * lastIterSol[index - n] -
+                                  valSubDiag * lastIterSol[index - 1] -
+                                  valSubDiag * lastIterSol[index + 1] - valSubDiag * lastIterSol[index + n]);
+
+
+
+
+        if(tid==0) {
+            iteration++;
+        }
+        __syncthreads();
+//        if(iteration%step==0) {
+//            calculateResidual_CUDA(cuda_actualIteration, cuda_lastIterSol, resiCuda,n);
+//            cudaMemcpy(&resi,resiCuda,sizeof(flouble),cudaMemcpyDeviceToHost);
+//
+//            cout<<iteration*2<<": "<<resi<<endl;
+//            if(resi<tol) {
+//                break;
+//            }
+//            resi=0;  // Reset resiCuda.....is there any better way?
+//            cudaMemcpy(resiCuda,&resi,sizeof(flouble),cudaMemcpyHostToDevice);
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
 
 
-__global__ void calculateResidual_1Core_CUDA(float *a, float *b, float *c) {
+__global__ void calculateResidual_1Core_CUDA(float *a, float *b, float *c, int n) {
     __shared__ float se[1024];
 
     int tid=threadIdx.x;
-    int bid=blockIdx.x;
-    int n=blockDim.x;
-    //   Calculate
-    se[tid]=fabsf(a[tid+bid*n]-b[tid+bid*n]);
-    __syncthreads();
+    int bid;
 
-    //   Reducto
-    int numActiveThreads=n/2;
-    while(numActiveThreads>0) {
-        if(tid<numActiveThreads) {
-            se[tid]=se[tid]+se[tid+numActiveThreads];
-        }
-        numActiveThreads=numActiveThreads/2;
+    printf("hi %d",tid);
+    return;
+
+    for(bid=0;bid<n;bid++) {
+        //   Calculate
+        se[tid] = fabsf(a[tid + bid * n] - b[tid + bid * n]);
         __syncthreads();
-    }
+
+        //   Reducto
+        int numActiveThreads = n / 2;
+        while (numActiveThreads > 0) {
+            if (tid < numActiveThreads) {
+                se[tid] = se[tid] + se[tid + numActiveThreads];
+            }
+            numActiveThreads = numActiveThreads / 2;
+            __syncthreads();
+        }
 
 
-    if(tid==0) {
-        atomicAdd(c,se[0]);
+        if (tid == 0) {
+            atomicAdd(c, se[0]);
+        }
     }
 }
 
-__global__ void calculateResidual_1Core_CUDA(double *a, double *b, double *c) {
-    __shared__ double se[1024];
+__global__ void calculateResidual_1Core_CUDA(double *a, double *b, double *c, int n) {
+    __shared__ float se[1024];
 
     int tid=threadIdx.x;
-    int bid=blockIdx.x;
-    int n=blockDim.x;
+    int bid;
 
-    // Calculate a.*b
-    se[tid]=fabsf(a[tid+bid*n]-b[tid+bid*n]);
-    __syncthreads();
+    printf("hi %d",tid);
+    return;
 
-    // Sum Reducto
-    int numActiveThreads=n/2;
-    while(numActiveThreads>0) {
-        if(tid<numActiveThreads) {
-            se[tid]=se[tid]+se[tid+numActiveThreads];
-        }
-        numActiveThreads=numActiveThreads/2;
+    for(bid=0;bid<n;bid++) {
+        //   Calculate
+        se[tid] = fabsf(a[tid + bid * n] - b[tid + bid * n]);
         __syncthreads();
-    }
+
+        //   Reducto
+        int numActiveThreads = n / 2;
+        while (numActiveThreads > 0) {
+            if (tid < numActiveThreads) {
+                se[tid] = se[tid] + se[tid + numActiveThreads];
+            }
+            numActiveThreads = numActiveThreads / 2;
+            __syncthreads();
+        }
 
 
-    if(tid==0) {
-        atomicAdd(c,se[0]);
+        if (tid == 0) {
+            atomicAdd(c, se[0]);
+        }
     }
 }
-
 
 
 
