0531d11e0cb82c1c78387189f51c825cfd7d81c2
Kronecker <benjamin.grabiger@uni-jena.de>
2018-06-11 20:38:44 +0200

    Aufg13c

--- Task4/aufg13d.cpp
+++ Task4/aufg13d.cpp
@@ -144,7 +144,7 @@ flouble* jacobiIterCuda_MultiGPU_CPU(int n, flouble valBoundary, int* numberOfIt
 
    cudaSetDevice(1);
    cudaMemcpy(actualIteration,cuda_lastIterSolD1,sizeof(flouble)*m,cudaMemcpyDeviceToHost);
-   saveMyMatrix(actualIteration, n/2+1,n,1,4);
+   saveMyMatrixAppend(&actualIteration[n],n/2,n,1,3,n/2);
 
 
  //   cudaMemcpy(actualIteration,cuda_actualIteration, sizeof(flouble)*nn, cudaMemcpyDeviceToHost);
--- Task4/mmain.cu
+++ Task4/mmain.cu
@@ -12,6 +12,7 @@
 flouble* initMatrixKonstant(int m,int n, flouble value  ) ;
 void displayMyMatrix(flouble* matrix, int m,int n);
 void saveMyMatrix(flouble* matrix, int m,int n, flouble h, int numberTask);
+void saveMyMatrixAppend(flouble* matrix, int m,int n, flouble h, int numberTask, int offset);
 
 //#include "aufg13a.cpp"
 //#include "aufg13b.cpp"  // An include-disaster waiting to happen ....
@@ -97,8 +98,34 @@ void saveMyMatrix(flouble* matrix, int m,int n, flouble h, int numberTask) {
 
 
 
+void saveMyMatrixAppend(flouble* matrix, int m,int n, flouble h, int numberTask, int offset) {
+    // h=1 for save indices
+    std::ofstream myfile;
+    if (numberTask == 0)
+        myfile.open("./results_a.dat",std::ofstream::out | std::ofstream::app);
+    else if (numberTask == 1)
+        myfile.open("./results_b.dat",std::ofstream::out | std::ofstream::app);
+    else if (numberTask == 2)
+        myfile.open("./results_c.dat",std::ofstream::out | std::ofstream::app);
+    else if (numberTask == 3)
+        myfile.open("./results_d.dat",std::ofstream::out | std::ofstream::app);
+    else
+        myfile.open("./results_temp.dat",std::ofstream::out | std::ofstream::app);
 
-
+    flouble x;
+    flouble y;
+    for (int i=0;i<m;i++) {
+        for (int j=0;j<n;j++) {
+            x=h*i;
+            y=h*(j+offset);
+            // printf("<%d %d %f>",x,y,matrix[i*m+j]);
+            myfile<<x<<" "<<y<<" "<<matrix[i*n+j]<<"\n";
+        }
+        myfile<<std::endl;
+        // printf(" \n");
+    }
+    myfile.close();
+}
 
 
 
